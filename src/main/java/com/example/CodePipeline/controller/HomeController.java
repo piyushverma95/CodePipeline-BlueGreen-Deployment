394dabf42531f6c1775b1fcebcf612b9aac1450c
Piyush Verma <piyushverma95@users.noreply.github.com>
2020-02-18 12:12:13 +0530

    Update HomeController.java

--- src/main/java/com/example/CodePipeline/controller/HomeController.java
+++ src/main/java/com/example/CodePipeline/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String hello() {
-		return "Good Afternoon! Trying Blue-Green Deployment";
+		return "Good Afternoon! Trying Blue-Green Deployment, Take 2";
 	}
 	
 }
